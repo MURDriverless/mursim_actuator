0b429a70284ed54fb1efac66dccd87dd4f8862c5
Joseph Leong <leong.joseph@me.com>
2020-09-19 22:06:51 +1000

    changed to grouped effort drive controller, performance improvements on init and publish

--- src/actuator_node.cpp
+++ src/actuator_node.cpp
@@ -4,6 +4,14 @@
 Actuator::Actuator(ros::NodeHandle n, std::string &veh_name, bool &equal_drive)
     : equal_drive(equal_drive), nh(n), veh_name(veh_name)
 {
+    // Setup message layout for publishing
+    drive_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    drive_msg.layout.dim[0].size = 4;
+    drive_msg.layout.dim[0].stride = 1;
+
+    steer_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    steer_msg.layout.dim[0].size = 2;
+    steer_msg.layout.dim[0].stride = 1;
 
     if (ros::ok())
      {
@@ -16,11 +24,8 @@ Actuator::Actuator(ros::NodeHandle n, std::string &veh_name, bool &equal_drive)
 
 int Actuator::launchPublishers()
 {
-    pub_drive_lf = nh.advertise<std_msgs::Float64>("/" + veh_name + DRIVE_LF_TOPIC, 1);
-    pub_drive_lr = nh.advertise<std_msgs::Float64>("/" + veh_name + DRIVE_LR_TOPIC, 1);
-    pub_drive_rf = nh.advertise<std_msgs::Float64>("/" + veh_name + DRIVE_RF_TOPIC, 1);
-    pub_drive_rr = nh.advertise<std_msgs::Float64>("/" + veh_name + DRIVE_RR_TOPIC, 1);
     pub_steer = nh.advertise<std_msgs::Float64MultiArray>("/" + veh_name + STEER_TOPIC, 1);
+    pub_drive = nh.advertise<std_msgs::Float64MultiArray>("/" + veh_name + DRIVE_TOPIC, 1);
 }
 
 int Actuator::launchSubscribers()
@@ -30,32 +35,34 @@ int Actuator::launchSubscribers()
 
 void Actuator::pushEqualDrive(const float &n_acc)
 {
-    float max_wheel_tq = MAX_TORQUE / 4;
-    std::cout << max_wheel_tq << ' ' << n_acc << std::endl;
+    float acc_msg = n_acc * MAX_WHEEL_TORQUE;
 
-    std_msgs::Float64 acc_msg;
-    acc_msg.data = max_wheel_tq * n_acc;
+    drive_msg.data.clear();
+    drive_msg.data.assign({acc_msg, acc_msg, acc_msg, acc_msg});
+    pub_drive.publish(drive_msg);
 
-    pub_drive_lf.publish(acc_msg);
-    pub_drive_lr.publish(acc_msg);
-    pub_drive_rf.publish(acc_msg);
-    pub_drive_rr.publish(acc_msg);
+    std::cout << "Drive message: ";
+    printMessage(drive_msg.data);
+    std::cout << std::endl;
 }
 
 void Actuator::pushSteer(const float &steer)
 {
-    std::vector<float> steer_cmd {steer, steer};
-    
-    std_msgs::Float64MultiArray steer_msg;
-    steer_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
-    steer_msg.layout.dim[0].size = steer_cmd.size();
-    steer_msg.layout.dim[0].stride = 1;
-
     steer_msg.data.clear();
-    steer_msg.data.insert(steer_msg.data.end(), steer_cmd.begin(), steer_cmd.end());
-    std::cout << steer_msg << std::endl;
-
+    steer_msg.data.assign({steer, steer});
     pub_steer.publish(steer_msg);
+
+    std::cout << "Steer message: ";
+    printMessage(steer_msg.data);
+    std::cout << std::endl;
+}
+
+void Actuator::printMessage(const std::vector<double>& cmd)
+{
+    for (auto &c: cmd)
+    {
+        std::cout << c << ' ';
+    }
 }
 
 void Actuator::spin()
